d1f08cb1f2bc885d492bbeea7de8d88077c5d69e
Vladimir Belov <belov.vladimir@mail.ru>
2024-09-16 23:31:51 +0300

    1.8.4
    1. Возможность выбора всех доступных календарей
    2. Количество событий при выборе типов событий

---
+++ app/src/main/java/org/vovka/birthdaycountdown/WidgetUpdateDailyReceiver.java
@@ -0,0 +1,46 @@
+/*
+ * *
+ *  * Created by Vladimir Belov on 17.07.2024, 15:29
+ *  * Copyright (c) 2018 - 2024. All rights reserved.
+ *  * Last modified 17.07.2024, 15:29
+ *
+ */
+
+package org.vovka.birthdaycountdown;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class WidgetUpdateDailyReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "WidgetUpdateDReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        try {
+
+            StringBuilder log = new StringBuilder();
+
+            ContactsEvents eventsData = ContactsEvents.getInstance();
+            if (eventsData.getContext() == null) eventsData.setContext(context);
+            eventsData.getPreferences();
+            eventsData.setLocale(true);
+            eventsData.getEvents(context);
+
+            //Переинициализируем обновления виджетов
+            eventsData.initWidgetUpdate(log);
+
+            //Посылаем сообщения на обновление виджетов
+            eventsData.updateWidgets(0, log);
+
+            if (log.length() > 0) ToastExpander.showDebugMsg(context, log.toString());
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+            ToastExpander.showDebugMsg(context, ContactsEvents.getMethodName(3) + Constants.STRING_COLON_SPACE + e);
+        }
+    }
+}
